d3704b616e66927190ad05e9b2721a6479e57fa4
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2018-12-07 12:46:48 +0100

    Add some error handling

--- gui-client/src/main/java/todolist/client/gui/AddTask.java
+++ gui-client/src/main/java/todolist/client/gui/AddTask.java
@@ -8,108 +8,132 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.text.NumberFormatter;
 import java.awt.*;
 import java.text.NumberFormat;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
 public class AddTask extends JFrame {
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        AddTask frame = new AddTask();
+        frame.setTitle("TodoList - Add Task");
+        frame.setVisible(true);
+    }
 
-		AddTask frame = new AddTask();
-		frame.setTitle("TodoList - Add Task");
-		frame.setVisible(true);
+    /**
+     * Create the frame.
+     */
+    public AddTask() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 388, 300);
+        JPanel contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 
-	}
+        JLabel dueDateLabel = new JLabel("Due Date");
+        dueDateLabel.setBounds(10, 11, 69, 22);
+        contentPane.add(dueDateLabel);
 
-	/**
-	 * Create the frame.
-	 */
-	public AddTask() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 388, 300);
-		JPanel contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+        // day
+        var intFormat = NumberFormat.getIntegerInstance();
+        var numberFormatter = new NumberFormatter(intFormat);
+        numberFormatter.setValueClass(Integer.class);
+        numberFormatter.setAllowsInvalid(false);
+        numberFormatter.setMinimum(1);
+        numberFormatter.setMaximum(31);
+        var dayField = new JFormattedTextField(numberFormatter);
+        dayField.setBounds(89, 12, 76, 20);
+        dayField.setColumns(10);
+        contentPane.add(dayField);
 
-		JLabel dueDateLabel = new JLabel();
-		dueDateLabel.setText("Due Date");
-		dueDateLabel.setBounds(10, 11, 69, 22);
-		contentPane.add(dueDateLabel);
+        // month
+        var monthChoice = new Choice();
+        List.of(Month.values())
+                .forEach(e -> monthChoice.add(e.name().toLowerCase()));
+        monthChoice.setBounds(171, 11, 93, 20);
+        contentPane.add(monthChoice);
 
-		// day
-		var intFormat = NumberFormat.getIntegerInstance();
-		var numberFormatter = new NumberFormatter(intFormat);
-		numberFormatter.setValueClass(Integer.class);
-		numberFormatter.setAllowsInvalid(false);
-		numberFormatter.setMinimum(1);
-		numberFormatter.setMaximum(31);
-		var dayField = new JFormattedTextField(numberFormatter);
-		dayField.setBounds(89, 12, 76, 20);
-		dayField.setColumns(10);
-		contentPane.add(dayField);
+        // year
+        var yearFormat = NumberFormat.getIntegerInstance();
+        var yearFormatter = new NumberFormatter(yearFormat);
+        yearFormatter.setValueClass(Integer.class);
+        yearFormatter.setAllowsInvalid(false);
+        yearFormatter.setMinimum(1);
+        var yearField = new JFormattedTextField(yearFormatter);
+        yearField.setBounds(271, 12, 75, 20);
+        yearField.setColumns(10);
+        contentPane.add(yearField);
 
-		// month
-		var monthChoice = new Choice();
-		List.of(Month.values()).forEach(e -> monthChoice.add(e.name().toLowerCase()));
-		monthChoice.setBounds(171, 11, 93, 20);
-		contentPane.add(monthChoice);
+        // importance label
+        var textLabel = new JLabel("Importance");
+        textLabel.setBounds(10, 44, 69, 22);
+        contentPane.add(textLabel);
 
-		// year
-		var yearFormat = NumberFormat.getIntegerInstance();
-		var yearFormatter = new NumberFormatter(yearFormat);
-		yearFormatter.setValueClass(Integer.class);
-		yearFormatter.setAllowsInvalid(false);
-		yearFormatter.setMinimum(1);
-		var yearField = new JFormattedTextField(yearFormatter);
-		yearField.setBounds(271, 12, 75, 20);
-		yearField.setColumns(10);
-		contentPane.add(yearField);
+        // importance choice
+        var importanceChoice = new Choice();
+        importanceChoice.add("none");
+        List.of(Importance.values())
+                .forEach(e -> importanceChoice.add(e.name().toLowerCase()));
+        importanceChoice.setBounds(89, 44, 151, 20);
+        contentPane.add(importanceChoice);
 
-		var textLabel = new JLabel();
-		textLabel.setText("Priority");
-		textLabel.setBounds(10, 44, 69, 22);
-		contentPane.add(textLabel);
+        // description
+        var descriptionLabel = new JLabel("Description");
+        descriptionLabel.setBounds(10, 77, 93, 20);
+        contentPane.add(descriptionLabel);
 
-		// importance
-		var importanceChoice = new Choice();
-		List.of(Importance.values()).forEach(e -> importanceChoice.add(e.name().toLowerCase()));
-		importanceChoice.setBounds(89, 44, 151, 20);
-		contentPane.add(importanceChoice);
+        var descriptionArea = new JTextArea();
+        descriptionArea.setBounds(10, 108, 336, 83);
+        contentPane.add(descriptionArea);
 
-		// description
-		var descriptionLabel = new JLabel();
-		descriptionLabel.setText("Description");
-		descriptionLabel.setBounds(10, 77, 93, 20);
-		contentPane.add(descriptionLabel);
+        // add button
+        var addButton = new JButton("Add");
+        addButton.setBounds(256, 215, 89, 23);
+        addButton.addActionListener(e -> {
+            var builder = new TaskBuilder();
 
-		JTextArea txtrTxtarea = new JTextArea();
-		txtrTxtarea.setText("");
-		txtrTxtarea.setBounds(10, 108, 336, 83);
-		contentPane.add(txtrTxtarea);
+            var description = descriptionArea.getText();
+            if (description.isEmpty()) {
+                // TODO popup
+                return;
+            } else {
+                builder.setDescription(description);
+            }
 
-		JButton btnAdd = new JButton("Add");
-		btnAdd.setBounds(256, 215, 89, 23);
-		btnAdd.addActionListener(e -> {
-			var description = descriptionLabel.getText();
-			var importance = Importance.valueOf(importanceChoice.getSelectedItem().toUpperCase());
+            if (!importanceChoice.getSelectedItem().equals("none")) {
+                var importance = Importance.valueOf(importanceChoice.getSelectedItem().toUpperCase());
+                builder.setImportance(importance);
+            }
 
-			var day = Integer.parseInt(dayField.getText());
-			var month = Month.valueOf(monthChoice.getSelectedItem().toUpperCase());
-			var year = Integer.parseInt(yearField.getText());
-			var dueDate = LocalDate.of(year, month, day);
+            var datePresent = !dayField.getText().isEmpty() && !yearField.getText().isEmpty();
+            if (datePresent) {
+                var day = Integer.parseInt(dayField.getText());
+                var month = Month.valueOf(monthChoice.getSelectedItem().toUpperCase());
+                var year = Integer.parseInt(yearField.getText());
 
-			var creationDate = LocalDate.now();
+                try {
+                    builder.setDueDate(LocalDate.of(year, month, day));
+                } catch (DateTimeException ex) {
+                    // TODO on invalid date popup maybe ??
+                    ex.printStackTrace();
+                }
+            }
 
-			var task = new TaskBuilder().setDescription(description).setCompleted(false).setCreationDate(creationDate)
-					.setDueDate(dueDate).setImportance(importance).build();
+            var creationDate = LocalDate.now();
 
-		});
-		// ADD BUTTON
-		contentPane.add(btnAdd);
-	}
+            builder.setCompleted(false)
+                    .setCreationDate(creationDate)
+                    .build();
+
+            // TODO send query
+
+            // TODO reset fields
+        });
+        contentPane.add(addButton);
+    }
 }
