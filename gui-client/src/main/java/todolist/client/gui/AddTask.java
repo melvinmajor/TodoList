060adcef9c58b29c127435b561019fdaf08b3b22
Liber Maxime <m.liber@students.ephec.be>
2018-12-12 16:01:22 +0100

    Fix problem with the window of AddTask

--- gui-client/src/main/java/todolist/client/gui/AddTask.java
+++ gui-client/src/main/java/todolist/client/gui/AddTask.java
@@ -29,7 +29,7 @@ public class AddTask extends JFrame {
 	 * Create the frame.
 	 */
 	public AddTask() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 450, 260);
 		JPanel contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
