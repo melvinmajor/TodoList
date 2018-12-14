f18643e97f7206861d514056aeb10e7cc09c82f9
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2018-12-14 16:01:38 +0100

    Add BaseEditScreen

---
+++ gui-client/src/main/java/todolist/client/gui/BaseEditScreen.java
@@ -0,0 +1,33 @@
+package todolist.client.gui;
+
+import todolist.common.Task;
+
+import javax.swing.*;
+import java.util.function.Consumer;
+
+public abstract class BaseEditScreen extends JFrame {
+    private final String type;
+    private final Consumer<Task> onSelectAction;
+    private final JButton actionButton;
+
+
+    public BaseEditScreen(String type, Consumer<Task> onSelectAction) {
+        this.type = type;
+        this.onSelectAction = onSelectAction;
+        this.actionButton = new JButton(type);
+
+        this.actionButton.addActionListener(e -> {
+            // TODO
+            Task task = null;
+
+            if (task != null) {
+                onSelectAction.accept(task);
+            }
+
+        });
+
+        setTitle("TodoList - " + type + " Task");
+    }
+
+
+}
