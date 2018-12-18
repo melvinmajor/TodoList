083fbf64973c240e97e680e0f0ef5f1234aed67a
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2018-12-18 01:58:19 +0100

    Locale independent date display

--- gui-client/src/main/java/todolist/client/gui/Main.java
+++ gui-client/src/main/java/todolist/client/gui/Main.java
@@ -1,5 +1,7 @@
 package todolist.client.gui;
 
+import java.util.Locale;
+
 public class Main {
 
     /**
@@ -8,6 +10,7 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         GuiClient.instance.setPort(8002);
         GuiClient.instance.run();
     }
--- gui-client/src/test/java/todolist/client/gui/util/DisplayNameTest.java
+++ gui-client/src/test/java/todolist/client/gui/util/DisplayNameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,21 +12,22 @@ class DisplayNameTest {
 
     @Test
     void dateNameTest() {
+        Locale.setDefault(Locale.US);
         assertEquals("15 December 2000", DisplayName.of(LocalDate.of(2000, Month.DECEMBER, 15)));
     }
 
     @Test
-    void enumNameTest(){
+    void enumNameTest() {
         assertEquals("December", DisplayName.of(Month.DECEMBER));
     }
 
     @Test
-    void booleanTrueNameTest(){
+    void booleanTrueNameTest() {
         assertEquals("V", DisplayName.of(true));
     }
 
     @Test
-    void booleanFalseNameTest(){
+    void booleanFalseNameTest() {
         assertEquals("X", DisplayName.of(false));
     }
 }
