eb7310245d73f59dce7b0f36f142654e9d543ec2
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2018-12-20 07:35:10 +0100

    Save server arguments

--- server/src/main/java/todolist/server/Main.java
+++ server/src/main/java/todolist/server/Main.java
@@ -1,25 +1,48 @@
 package todolist.server;
 
-import java.util.List;
+import argparsing.ArgParser;
 
-/**
- * the main method.
- * @param args the args, --port followed by a port number
- */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static argparsing.OptionFactory.newFlag;
+import static argparsing.OptionFactory.newOption;
+
+
 public class Main {
+    /**
+     * the main method.
+     *
+     * @param args the args, --port or -p followed by a port number
+     *             and -s or --save to save other args
+     */
     public static void main(String[] args) {
-        List<String> argsList = List.of(args);
+        var pathString = "server-config.cfg";
+        var configPath = Path.of(pathString);
 
-        int port = 8002;
-        int i = argsList.indexOf("--port");
-        if (i != -1 && i + 1 < argsList.size()) {
-            String maybePort = argsList.get(i + 1);
+        var saveFlg = newFlag("s", "save");
+        var portOpt = newOption("p", "port");
+        new ArgParser(portOpt, saveFlg)
+                .addFileSource(pathString)
+                .addSource(args)
+                .parse();
+
+        if (saveFlg.isPresent()) {
+            var argsWithoutSaveFlag = Arrays.stream(args)
+                    .filter(s -> !s.equals("-s") && !s.equals("--save"))
+                    .collect(Collectors.joining(" "));
+
             try {
-                port = Integer.parseInt(maybePort);
-            } catch (NumberFormatException e) {
-                System.err.println("Invalid port");
+                Files.writeString(configPath, argsWithoutSaveFlag);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+
+        int port = portOpt.getOptionalInt().orElse(8002);
 
         new Server(port).run();
     }
