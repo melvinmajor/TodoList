14f86b833dc4184525a8cccd262599f2e6377391
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2018-12-20 23:50:01 +0100

    Add proper header (?)

--- server/src/main/java/todolist/server/JsonHttpServer.java
+++ server/src/main/java/todolist/server/JsonHttpServer.java
@@ -1,5 +1,6 @@
 package todolist.server;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import todolist.server.serialization.SerializableTask;
 import todolist.server.serialization.TaskSerialization;
@@ -8,8 +9,18 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class JsonHttpServer {
+    private static final String HEADER_CONTENT_TYPE = "Content-Type";
+    private static final String JSON_CONTENT_TYPE = "application/json; charset=UTF-8";
+
+    private static final String METHOD_GET = "GET";
+    private static final String METHOD_OPTIONS = "OPTIONS";
+    private static final String ALLOWED_METHODS = METHOD_GET + "," + METHOD_OPTIONS;
+
+    private static final String HEADER_ALLOW = "Allow";
+
     private final int port;
     private HttpServer httpServer;
+
 
     public JsonHttpServer(int port) {
         this.port = port;
@@ -19,18 +30,32 @@ public class JsonHttpServer {
         Server.logger.info("Starting http server @ port " + port);
         try {
             httpServer = HttpServer.create(new InetSocketAddress(port), 0);
-            httpServer.createContext("/tasks", exchange -> {
-                byte[] response = getJsonTasks().getBytes();
-                exchange.sendResponseHeaders(200, response.length);
-                var outputStream = exchange.getResponseBody();
-                outputStream.write(response);
-                outputStream.close();
-            });
+            httpServer.createContext("/tasks", this::sendResponse);
             httpServer.setExecutor(null);
             httpServer.start();
             Server.logger.info("Started http server @ port " + port);
         } catch (IOException e) {
             Server.logger.error(e.getMessage());
+        }
+    }
+
+    private void sendResponse(HttpExchange exchange) throws IOException {
+        switch (exchange.getRequestMethod().toUpperCase()) {
+            case METHOD_GET:
+                exchange.getResponseHeaders().set(HEADER_CONTENT_TYPE, JSON_CONTENT_TYPE);
+                var response = getJsonTasks().getBytes();
+                exchange.sendResponseHeaders(200, response.length);
+                var outputStream = exchange.getResponseBody();
+                outputStream.write(response);
+                outputStream.close();
+                break;
+            case METHOD_OPTIONS:
+                exchange.getResponseHeaders().set(HEADER_ALLOW, ALLOWED_METHODS);
+                exchange.sendResponseHeaders(200, -1);
+                break;
+            default:
+                exchange.getResponseHeaders().set(HEADER_ALLOW, ALLOWED_METHODS);
+                exchange.sendResponseHeaders(405, -1);
         }
     }
 
